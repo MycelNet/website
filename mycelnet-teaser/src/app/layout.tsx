7dc39d700e1f1dab59f81b5799bae401d3085463
Greg Horejsi <greg@mycelnet.tech>
2023-09-03 21:10:38 -0500

    chore: house keeping

--- mycelnet-teaser/src/app/layout.tsx
+++ mycelnet-teaser/src/app/layout.tsx
@@ -1,26 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-})
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: 'MycelNet : Shroom-Ops',
-  description: 'Introducing MycelNet, a decentralized mesh hosting network for the people.',
-}
+  title: "MycelNet : Shroom-Ops",
+  description:
+    "Introducing MycelNet, a decentralized mesh platform for the people.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={roboto.className}>{children}</body>
     </html>
-  )
+  );
 }
